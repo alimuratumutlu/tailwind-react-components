1b216662871bc9c7857422c5ca1a5f7ca4a09d2d
Murat Umutlu <murat@muum.dev>
2023-01-05 14:45:30 +0300

    primary button changed

--- atoms/buttons/PrimaryButton.js
+++ atoms/buttons/PrimaryButton.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function PrimaryButton({ children, disabled, onClick }) {
+export default function PrimaryButton({ children, disabled, onClick,color }) {
 	return (
 		<button
 			type="submit"
-			className="w-full block rounded-lg px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+			className={`w-full rounded-lg px-7 py-3 bg-indigo-600 text-white font-medium text-sm leading-snug shadow-md hover:bg-${color}-700 hover:shadow-lg focus:bg-${color}-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-${color}-800 active:shadow-lg transition duration-150 ease-in-out`}
 			disabled={disabled}
 			onClick={onClick}
 		>
