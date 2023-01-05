4e3137dd2d2828d22d0b7199c16e5120bde8ab84
Murat Umutlu <murat@muum.dev>
2023-01-05 15:32:41 +0300

    component prop changed

--- atoms/buttons/PrimaryButton.js
+++ atoms/buttons/PrimaryButton.js
@@ -1,6 +1,6 @@
 import React, {useMemo} from "react";
 
-export default function PrimaryButton({ children, disabled, onClick, color }) {
+export default function PrimaryButton( children, disabled, onClick, color ) {
 
 	const buttonClass = useMemo(() => {
 		let buttonClasses = [`w-full inline-grid rounded-lg px-7 py-3 mb-4 font-medium text-sm leading-snug shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out bg-${color}-500 hover:bg-${color}-600 focus:bg-${color}-600 active:bg-${color}-700 `];
