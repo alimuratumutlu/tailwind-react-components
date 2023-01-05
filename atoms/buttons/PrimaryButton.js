05be4c099ad33837e4d3ea6d94fe3dc649c8caf8
Murat Umutlu <murat@muum.dev>
2023-01-05 14:55:33 +0300

    button class is now supporting color

--- atoms/buttons/PrimaryButton.js
+++ atoms/buttons/PrimaryButton.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, {useMemo} from "react";
 
-export default function PrimaryButton({ children, disabled, onClick,color }) {
+export default function PrimaryButton({ children, disabled, onClick, color }) {
+
+	const buttonClass = useMemo(() => {
+		let buttonClasses = [`w-full inline-grid rounded-lg px-7 py-3  text-white font-medium text-sm leading-snug shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out`];
+
+		if (color) {
+			buttonClasses.push(`bg-${color}-600 hover:bg-${color}-700 focus:bg-${color}-700 active:bg-${color}-800 `);
+		}
+
+		return buttonClasses.join(" ");
+	}, [color]);
+
 	return (
 		<button
 			type="submit"
-			className={`w-full rounded-lg px-7 py-3 bg-indigo-600 text-white font-medium text-sm leading-snug shadow-md hover:bg-${color}-700 hover:shadow-lg focus:bg-${color}-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-${color}-800 active:shadow-lg transition duration-150 ease-in-out`}
+			className={buttonClass}
 			disabled={disabled}
 			onClick={onClick}
 		>
