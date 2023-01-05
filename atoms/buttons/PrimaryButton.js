5785edfdd310e5110de4b9faa958e283b64bf775
Murat Umutlu <murat@muum.dev>
2023-01-05 15:02:21 +0300

    text update

--- atoms/buttons/PrimaryButton.js
+++ atoms/buttons/PrimaryButton.js
@@ -3,11 +3,15 @@ import React, {useMemo} from "react";
 export default function PrimaryButton({ children, disabled, onClick, color }) {
 
 	const buttonClass = useMemo(() => {
-		let buttonClasses = [`w-full inline-grid rounded-lg px-7 py-3  text-white font-medium text-sm leading-snug shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out`];
+		let buttonClasses = [`w-full inline-grid rounded-lg px-7 py-3 mb-4 font-medium text-sm leading-snug shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out`];
 
-		if (color) {
-			buttonClasses.push(`bg-${color}-600 hover:bg-${color}-700 focus:bg-${color}-700 active:bg-${color}-800 `);
+		if (color === "white") {
+			buttonClasses.push(`bg-${color}-600 hover:bg-${color}-700 focus:bg-${color}-700 active:bg-${color}-800 text-gray-900`);
+		} else {
+			buttonClasses.push(`bg-${color}-500 hover:bg-${color}-600 focus:bg-${color}-600 active:bg-${color}-700 text-white `);
 		}
+
+		console.log(buttonClasses.join(" "), "buttonclasses")
 
 		return buttonClasses.join(" ");
 	}, [color]);
